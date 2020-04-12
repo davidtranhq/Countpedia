d5dedf9868adb2e7e2dc18c69e058b6ccec76798
davidtranhq <davidtranhq@gmail.com>
2020-04-12 15:50:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function createList()
         let linkNode = document.createElement("a")
         let linkText = document.createTextNode(name);
         linkNode.appendChild(linkText);
-        linkNode.href = "/countpedia/pages/" + link + ".html";
+        linkNode.href = window.location.origin + "pages/" + link + ".html";
         node.appendChild(linkNode);
         document.getElementById("character_list").appendChild(node);
     }
